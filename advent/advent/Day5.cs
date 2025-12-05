c4edc5db8e384d0f97806ea4575badae158d60a6
Ross Bentley-Davies <ross@bentley-davies.co.uk>
2025-12-05 10:28:50 +0000

    Day 5 part 2

--- advent/advent/Day5.cs
+++ advent/advent/Day5.cs
@@ -25,20 +25,67 @@ public static class Day5
             }
         }
 
-        var freshIngredientCount = 0;
-        foreach (var ingredient in ingredients)
+        long totalFreshIngredients = 0;
+        var firstNumber = ranges.Min(r => r.Min);
+        var lastNumber = ranges.Max(r => r.Max);
+        var orderedRanges = ranges.OrderBy(x => x.Min).ThenBy(x => x.Max).ToArray();
+        long previousMin = 0;
+        long previousMax = 0;
+        foreach (var range in orderedRanges)
         {
-            foreach (var range in ranges)
+            if (previousMin == range.Min)
             {
-                if (range.IsInRange(ingredient))
-                {
-                    freshIngredientCount++;
-                    break;
-                }
+                Console.WriteLine($"duplicate range starts:{range.Min}");
             }
+            if (previousMax == range.Max)
+            {
+                Console.WriteLine($"duplicate range ends:{range.Max}");
+            }
+            previousMin = range.Min;
+            if (previousMax >= range.Max)
+            {
+                //new range completely overlapped by previous range
+                continue;
+            }
+            if (previousMax < range.Max && range.Min <= previousMax)
+            {
+                //there's an overlap
+                totalFreshIngredients += (range.Max - previousMax);
+                
+            }
+            else
+            {
+                totalFreshIngredients += (range.Max - range.Min + 1);
+            }
+            previousMax = range.Max;
         }
+        var freshIngredientCount = 0;
+        // foreach (var ingredient in ingredients)
+        // {
+        //     foreach (var range in ranges)
+        //     {
+        //         if (range.IsInRange(ingredient))
+        //         {
+        //             freshIngredientCount++;
+        //             break;
+        //         }
+        //     }
+        // }
+
+        // for (var ingredient = firstNumber; ingredient <= lastNumber; ingredient++)
+        // {
+        //     foreach (var range in ranges)
+        //     {
+        //         if (range.IsInRange(ingredient))
+        //         {
+        //             freshIngredientCount++;
+        //             break;
+        //         }
+        //     }
+        // }
         Console.WriteLine($"There are {ranges.Count} ranges and {ingredients.Count} ingredients.");
         Console.WriteLine($"There are {freshIngredientCount} fresh ingredients.");
+        Console.WriteLine($"There are {totalFreshIngredients} fresh ingredients altogether.");
     }
     private class Range
     {
