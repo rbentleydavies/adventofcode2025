1fe614a654ecb79a37061449591b52d3d8a48b24
Ross Bentley-Davies <ross@bentley-davies.co.uk>
2025-12-08 13:29:43 +0000

    Day 8 Part 1

--- advent/advent/Day7.cs
+++ advent/advent/Day7.cs
@@ -9,51 +9,82 @@ public static class Day7
     {
         var lines = File.ReadAllLines(fileName).Select(x=>x.ToCharArray()).ToArray();
 
-        var totalSplits = 0;
-        while (lines.Length > 1)
-        {
-            (lines, var splits) = ProgressBeams(lines);
-            totalSplits += splits;
-        }
+        var totalSplits = ProgressBeams(lines);
+            
         Console.WriteLine($"Beam has split: {totalSplits} times");
+        // for (var i = 0; i < lines.Length; i++)
+        // {
+        //     for (var j = 0; j < lines[i].Length; j++)
+        //     {
+        //         if(cacheSteps.ContainsKey((j, i)))
+        //             Console.WriteLine($"{i}, {j}   :{cacheSteps[(j,i)]}");
+        //     }
+        // }
 
     }
-
-    private static (char[][], int) ProgressBeams(char[][] lines)
+    private static Dictionary<(int,int),long> cacheSteps = new();
+    private static long ProgressBeams(char[][] lines)
     {
+        if (lines.Length <= 1) return 1;
         Console.WriteLine($"There are: {lines.Length} steps to go");
         Console.WriteLine(new string(lines[0]));
         Console.WriteLine(new string(lines[1]));
-        var splits = 0;
+        long splits = 1;
         for (var c=0; c<  lines[0].Length; c++)
         {
             if (lines[0][c] == 'S')
             {
+                Console.WriteLine($"The beam is at {c}");
+                if (cacheSteps.ContainsKey((c,lines.Length))) return cacheSteps[(c,lines.Length)];
                 //found the beam, can we move it down?
                 if (lines[1][c] != '^')
                 {
-                    lines[1][c] = 'S';
+                    Console.WriteLine($"Moving the beam down one");
+                    var nextStepMap = NextStepMap(lines);
+                    nextStepMap[0][c] = 'S';
+                    splits = ProgressBeams(nextStepMap);
                 }
                 else
                 {
-                    splits++;
+                    Console.WriteLine($"Splitting the beam");
+                    //splits++;
                     if (c == 0 || lines[1][c - 1] == '^')
                     {
-                        Console.Write($"Can't split, no space before {c}");
+                        Console.WriteLine($"Can't split, no space before {c}");
                     }
-
-                    lines[1][c - 1] = 'S';
-
+                    var leftTimeline = NextStepMap(lines);
+                    
+                    leftTimeline[0][c - 1] = 'S';
+                    var leftTimelineSplits = ProgressBeams(leftTimeline);
+                    
                     if (c == lines[1].Length - 1 || lines[1][c + 1] == '^')
                     {
-                        Console.Write($"Can't split, no space after {c}");
+                        Console.WriteLine($"Can't split, no space after {c}");
                     }
+                    var rightTimeline = NextStepMap(lines);
                     
-                    lines[1][c + 1] = 'S';
+                    rightTimeline[0][c + 1] = 'S';
+                    var rightTimelineSplits = ProgressBeams(rightTimeline);
+                    
+                    splits = rightTimelineSplits + leftTimelineSplits;
                 }
+                cacheSteps[(c,lines.Length)] = splits;
             }
         }
 
-        return (lines.Skip(1).ToArray(), splits);
+        return splits;
+    }
+
+    private static char[][] NextStepMap(char[][] lines)
+    {
+        var output = new char[lines.Length - 1][];
+        for (var i = 1; i < lines.Length; i++)
+        {
+            output[i-1] = new char[lines[i].Length];
+            
+            Array.Copy(lines[i],output[i-1], lines[i].Length); 
+        }
+        Console.WriteLine($"New map has {output.Length} steps, old map had {lines.Length} steps");
+        return output;
     }
 }
--- advent/advent/Day8.cs
+++ advent/advent/Day8.cs
@@ -8,8 +8,118 @@ public static class Day8
     public static void Run(string fileName)
     {
         var lines = File.ReadAllLines(fileName);
-        foreach (var line in lines)
+        var junctions = lines.Select(x => new Junction(x)).ToArray();
+        var shortestDistance = double.MaxValue;
+        var shortestJunctions = new Junction[2];
+        var circuits = new List<List<Junction>>();
+        var connections = new List<Junction[]>();
+        for (var i = 0; i < 1000; i++)
         {
+            foreach (var junctionA in junctions)
+            {
+                foreach (var junctionB in junctions)
+                {
+                    if (junctionA.Equals(junctionB)) continue;
+                    if (connections.Any(x => x.Any(y => y.Equals(junctionA))
+                                          && x.Any(y => y.Equals(junctionB)))) continue;
+                    var distance = DistanceBetweenJunctions(junctionA, junctionB);
+                    if (distance < shortestDistance)
+                    {
+                        shortestDistance = distance;
+                        shortestJunctions = [junctionA, junctionB];
+                    }
+                }
+            }
+
+            //Console.WriteLine($"Shortest distance: {shortestDistance}");
+            Console.WriteLine($"Shortest Junctions: {shortestJunctions[0]},  {shortestJunctions[1]}");
+            connections.Add(shortestJunctions);
+            AddToCircuit(circuits, shortestJunctions);
+            shortestDistance = double.MaxValue;
+            Console.WriteLine($"There are {circuits.Count} circuits");
+            foreach (var circuit in circuits.OrderByDescending(c=>c.Count))
+            {
+                Console.Write($"{circuit.Count} ,");
+            }
+            Console.WriteLine();
+            
         }
+        Console.WriteLine(circuits.OrderByDescending(c=>c.Count).Take(3).Aggregate(1,(a,b)=>a * b.Count));
+
+        
+        Console.WriteLine(junctions.Length);
+    }
+
+    public static void AddToCircuit(List<List<Junction>> circuits, Junction[] junctions)
+    {
+        var existingCircuits =
+            circuits.Where(x => x.Any(y => y.Equals(junctions[0]) || y.Equals(junctions[1])));
+        if (existingCircuits.Count() == 2)
+        {
+            //join the circuits together
+            existingCircuits.First().AddRange(existingCircuits.Last());
+            existingCircuits.Last().Clear();
+        }
+        var existingCircuit = existingCircuits.FirstOrDefault();
+        if (existingCircuit != null)
+        {
+            if (!existingCircuit.Any(y => y.Equals(junctions[0])))
+            {
+                existingCircuit.Add(junctions[0]);
+                Console.WriteLine($"Circuit {junctions[0]} added to existing circuit");
+                foreach (var junction in existingCircuit)
+                {
+                    Console.WriteLine($" - {junction}");
+                }
+                
+            }
+
+            if (!existingCircuit.Any(y => y.Equals(junctions[1])))
+            {
+                existingCircuit.Add(junctions[1]);
+                Console.WriteLine($"Circuit {junctions[1]} added to existing circuit:");
+                foreach (var junction in existingCircuit)
+                {
+                    Console.WriteLine($" - {junction}");
+                }
+            }
+        }
+        else
+        {
+            circuits.Add([junctions[0], junctions[1]]);
+            Console.WriteLine($"New circuit created");
+        }
+    }
+
+    public static double DistanceBetweenJunctions(Junction junctionA, Junction junctionB)
+    {
+        return Math.Sqrt(Math.Pow(junctionA.x - junctionB.x, 2) + Math.Pow(junctionA.y - junctionB.y, 2) +
+                         Math.Pow(junctionA.z - junctionB.z, 2));
+    }
+
+    public class Junction
+    {
+        public Junction(string line)
+        {
+            var parts = line.Split(',');
+            if (parts.Length != 3) throw new Exception();
+            x = int.Parse(parts[0]);
+            y = int.Parse(parts[1]);
+            z = int.Parse(parts[2]);
+        }
+
+        public override string ToString()
+        {
+            return $"{x},{y},{z}";
+        }
+
+        public bool Equals(Junction obj)
+        {
+            return x == obj.x && y == obj.y && z == obj.z;
+        }
+
+        public int x = 0;
+        public int y = 0;
+        public int z = 0;
     }
 }
